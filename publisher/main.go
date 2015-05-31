67fe7ef4d0e220646c5801096355b5d4a585ba8f
andrew webber (personal) <andrewvwebber@googlemail.com>
2015-05-31 19:05:10 +0200

    Scaleout test

--- publisher/main.go
+++ publisher/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"reflect"
-	"time"
 
 	"github.com/andrewwebber/cqrs"
 
@@ -32,14 +31,14 @@ func main() {
 	// Publish a simple event to the exchange http://www.rabbitmq.com/tutorials/tutorial-three-python.html
 	log.Println("Publishing Commands")
 
-	for {
-		time.Sleep(1 * time.Second)
-		for i := 0; i < 10; i++ {
-			if err := bus.PublishCommands([]cqrs.Command{cqrs.Command{
-				CommandType: commandType.String(),
-				Body:        scaleout.SampleCommand{"rabbit_TestCommandBus"}}}); err != nil {
-				failOnError(err, "publish commands")
-			}
+	//for {
+	//time.Sleep(1 * time.Second)
+	for i := 0; i < 10; i++ {
+		if err := bus.PublishCommands([]cqrs.Command{cqrs.Command{
+			CommandType: commandType.String(),
+			Body:        scaleout.SampleCommand{"rabbit_TestCommandBus"}}}); err != nil {
+			failOnError(err, "publish commands")
 		}
 	}
+	//}
 }
--- subscriber/main.go
+++ subscriber/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/andrewwebber/cqrs"
+	"github.com/couchbaselabs/go-couchbase"
 
 	"github.com/andrewwebber/cqrs/rabbit"
 
@@ -34,7 +36,7 @@ func main() {
 	// and receiving commands from the queue
 	receiveCommandChannel := make(chan cqrs.CommandTransactedAccept)
 	// Start receiving events by passing these channels to the worker thread (go routine)
-	if err := bus.ReceiveCommands(cqrs.CommandReceiverOptions{commandTypeCache, closeChannel, errorChannel, receiveCommandChannel, true}); err != nil {
+	if err := bus.ReceiveCommands(cqrs.CommandReceiverOptions{commandTypeCache, closeChannel, errorChannel, receiveCommandChannel, false}); err != nil {
 		failOnError(err, "Receive")
 	}
 
@@ -45,11 +47,97 @@ func main() {
 		// This should eventually call an event handler. See cqrs.NewVersionedEventDispatcher()
 		case command := <-receiveCommandChannel:
 			sampleCommand := command.Command.Body.(scaleout.SampleCommand)
-			log.Println(sampleCommand.Message)
-			command.ProcessedSuccessfully <- true
+			err := updateValueInCouchbaseV2(sampleCommand.Message)
+			if err == nil {
+				command.ProcessedSuccessfully <- true
+			} else {
+				log.Println(err)
+				command.ProcessedSuccessfully <- false
+			}
+
 			// Receiving on this channel signifys an error has occured work processor side
 		case err := <-errorChannel:
 			failOnError(err, "Error")
 		}
 	}
+}
+
+type DataObject struct {
+	Message string
+	Count   int
+}
+
+func updateValueInCouchbase(message string) error {
+	client, err := couchbase.Connect("http://localhost:8091/")
+	if err != nil {
+		return err
+	}
+
+	pool, err := client.GetPool("default")
+	if err != nil {
+		return err
+	}
+
+	bucket, err := pool.GetBucket("cqrs")
+	if err != nil {
+		return err
+	}
+
+	var dataObject DataObject
+	dataObjectKey := "cqrs-scaleout:dataobject"
+	err = bucket.Get(dataObjectKey, &dataObject)
+	if err != nil {
+		if !IsNotFoundError(err) {
+			return err
+		}
+
+		dataObject = DataObject{Message: message, Count: 0}
+	}
+
+	dataObject.Count = dataObject.Count + 1
+	log.Println(dataObject.Count)
+	return bucket.Set(dataObjectKey, 0, &dataObject)
+}
+
+func updateValueInCouchbaseV2(message string) error {
+	client, err := couchbase.Connect("http://localhost:8091/")
+	if err != nil {
+		return err
+	}
+
+	pool, err := client.GetPool("default")
+	if err != nil {
+		return err
+	}
+
+	bucket, err := pool.GetBucket("cqrs")
+	if err != nil {
+		return err
+	}
+
+	var dataObject DataObject
+	dataObjectKey := "cqrs-scaleout:dataobject"
+	var cas uint64
+	err = bucket.Gets(dataObjectKey, &dataObject, &cas)
+	if err != nil {
+		if !IsNotFoundError(err) {
+			return err
+		}
+
+		dataObject = DataObject{Message: message, Count: 0}
+	}
+
+	dataObject.Count = dataObject.Count + 1
+	log.Println(dataObject.Count)
+	log.Println(cas)
+	return bucket.Cas(dataObjectKey, 0, cas, &dataObject)
+}
+
+func IsNotFoundError(err error) bool {
+	// No error?
+	if err == nil {
+		return false
+	}
+
+	return strings.Contains(err.Error(), "Not found")
 }
